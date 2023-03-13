425c973df4d78617b2426e1a624b6143e75be2de
Christina Mathipa <1856076@students.wits.ac.za>
2023-03-13 21:37:17 +0200

    Section 1 to 4

---
+++ hello.js
@@ -0,0 +1,14 @@
+'use strict'
+const message = 'Hi Christina!'
+console.log(message)
+
+let sum = 0
+
+for (let i = 0; i < 6; i++) {
+  sum += 1
+}
+
+console.log(sum)
+
+if (sum % 2 === 0) console.log('Sum is even')
+else console.log("It's odd")
